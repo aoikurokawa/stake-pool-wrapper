b1daa407f712ec3d863574189383d586686f7639
aoikurokawa <ht.so.ik0102@gmail.com>
2025-05-08 05:40:46 +0900

    fix: update:

--- cli/src/stake_pool_wrapper_handler.rs
+++ cli/src/stake_pool_wrapper_handler.rs
@@ -5,7 +5,7 @@ use solana_sdk::{
     pubkey::Pubkey,
     signer::Signer,
 };
-use spl_stake_pool::instruction::update_stake_pool_balance;
+use spl_stake_pool::instruction::{deposit_sol, update_stake_pool_balance};
 use stake_pool_wrapper_sdk::instruction::StakePoolWrapperInstruction;
 
 use crate::{
@@ -37,6 +37,9 @@ impl StakePoolWrapperCliHandler {
                 action: StakePoolWrapperActions::DepositSol,
             } => self.deposit_sol(),
             StakePoolWrapperCommands::StakePoolWrapper {
+                action: StakePoolWrapperActions::WrapperDepositSol,
+            } => self.wrapper_deposit_sol(),
+            StakePoolWrapperCommands::StakePoolWrapper {
                 action: StakePoolWrapperActions::UpdateStakePoolBalance,
             } => self.update_stake_pool_balance(),
         }
@@ -47,6 +50,47 @@ impl StakePoolWrapperCliHandler {
 impl StakePoolWrapperCliHandler {
     /// Deposit SOL
     pub fn deposit_sol(&self) -> anyhow::Result<()> {
+        let signer = self.signer()?;
+        let admin = signer.pubkey();
+
+        let stake_pool = Pubkey::from_str("Jito4APyf642JPZPx3hGc6WWJ8zPKtRbRs4P815Awbb").unwrap();
+        let stake_pool_withdraw_authority =
+            Pubkey::from_str("6iQKfEyhr3bZMotVkW6beNZz5CPAkiwvgV2CTje9pVSS").unwrap();
+        let reserve_stake_pool =
+            Pubkey::from_str("rrWBQqRqBXYZw3CmPCCcjFxQ2Ds4JFJd7oRQJ997dhz").unwrap();
+        let depositer = admin;
+        let user = Pubkey::from_str("22Mjmaea25LDrpEQyJonfV6ybkrDcxGDsoCqUH39Cw9m").unwrap();
+        let fee = Pubkey::from_str("DH7tmjoQ5zjqcgfYJU22JqmXhP5EY1tkbYpgVWUS2oNo").unwrap();
+        let referral_fee =
+            Pubkey::from_str("22Mjmaea25LDrpEQyJonfV6ybkrDcxGDsoCqUH39Cw9m").unwrap();
+        let pool_mint = Pubkey::from_str("J1toso1uCk3RLmjorhTtrVwY9HJ7X8V9yYac6Y7kGCPn").unwrap();
+        let system_program = Pubkey::from_str("11111111111111111111111111111111").unwrap();
+        let token_program =
+            Pubkey::from_str("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA").unwrap();
+        let stake_pool_program =
+            Pubkey::from_str("SPoo1Ku8WFXoNDMHPsrGSTSG1Y47rzgn41SLUNakuHy").unwrap();
+
+        let ix = deposit_sol(
+            &stake_pool_program,
+            &stake_pool,
+            &stake_pool_withdraw_authority,
+            &reserve_stake_pool,
+            &depositer,
+            &user,
+            &fee,
+            &referral_fee,
+            &pool_mint,
+            &token_program,
+            1,
+        );
+
+        let ixs = [ix];
+        self.process_transaction(&ixs, &signer.pubkey(), &[signer])?;
+
+        Ok(())
+    }
+    /// Deposit SOL
+    pub fn wrapper_deposit_sol(&self) -> anyhow::Result<()> {
         let signer = self.signer()?;
         let admin = signer.pubkey();
 
