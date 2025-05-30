768c14a0c653950874e385738590a804038e8264
aoikurokawa <ht.so.ik0102@gmail.com>
2025-05-31 07:54:58 +0900

    fix: update

--- cli/src/stake_pool_wrapper_handler.rs
+++ cli/src/stake_pool_wrapper_handler.rs
@@ -67,38 +67,6 @@ impl StakePoolWrapperCliHandler {
         let _admin = signer.pubkey();
 
         let stake_pool = Pubkey::from_str("Jito4APyf642JPZPx3hGc6WWJ8zPKtRbRs4P815Awbb").unwrap();
-        // let stake_pool_withdraw_authority =
-        //     Pubkey::from_str("6iQKfEyhr3bZMotVkW6beNZz5CPAkiwvgV2CTje9pVSS").unwrap();
-        // let reserve_stake_pool =
-        //     Pubkey::from_str("rrWBQqRqBXYZw3CmPCCcjFxQ2Ds4JFJd7oRQJ997dhz").unwrap();
-        // let depositer = admin;
-        // let user = Pubkey::from_str("22Mjmaea25LDrpEQyJonfV6ybkrDcxGDsoCqUH39Cw9m").unwrap();
-        // let fee = Pubkey::from_str("DH7tmjoQ5zjqcgfYJU22JqmXhP5EY1tkbYpgVWUS2oNo").unwrap();
-        // let referral_fee =
-        //     Pubkey::from_str("22Mjmaea25LDrpEQyJonfV6ybkrDcxGDsoCqUH39Cw9m").unwrap();
-        // let pool_mint = Pubkey::from_str("J1toso1uCk3RLmjorhTtrVwY9HJ7X8V9yYac6Y7kGCPn").unwrap();
-        // let _system_program = Pubkey::from_str("11111111111111111111111111111111").unwrap();
-        // let token_program =
-        //     Pubkey::from_str("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA").unwrap();
-        // let stake_pool_program =
-        //     Pubkey::from_str("SPoo1Ku8WFXoNDMHPsrGSTSG1Y47rzgn41SLUNakuHy").unwrap();
-
-        // let ix = deposit_sol(
-        //     &stake_pool_program,
-        //     &stake_pool,
-        //     &stake_pool_withdraw_authority,
-        //     &reserve_stake_pool,
-        //     &depositer,
-        //     &user,
-        //     &fee,
-        //     &referral_fee,
-        //     &pool_mint,
-        //     &token_program,
-        //     amount * LAMPORTS_PER_SOL,
-        // );
-
-        // let ixs = [ix];
-        // self.process_transaction(&ixs, &signer.pubkey(), &[signer])?;
 
         let rpc_client = self.get_rpc_client();
         let acc_data = rpc_client.get_account_data(&stake_pool).unwrap();
@@ -308,20 +276,6 @@ impl StakePoolWrapperCliHandler {
             Pubkey::from_str("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA").unwrap();
         let stake_pool_program =
             Pubkey::from_str("SPoo1Ku8WFXoNDMHPsrGSTSG1Y47rzgn41SLUNakuHy").unwrap();
-
-        // let accounts = vec![
-        //     AccountMeta::new(stake_pool, false),
-        //     AccountMeta::new_readonly(stake_pool_withdraw_authority, false),
-        //     AccountMeta::new(reserve_stake_pool, false),
-        //     AccountMeta::new(depositer, true),
-        //     AccountMeta::new(user, false),
-        //     AccountMeta::new(fee, false),
-        //     AccountMeta::new(referral_fee, false),
-        //     AccountMeta::new(pool_mint, false),
-        //     AccountMeta::new_readonly(system_program, false),
-        //     AccountMeta::new_readonly(token_program, false),
-        //     AccountMeta::new_readonly(stake_pool_program, false),
-        // ];
 
         let ix = update_stake_pool_balance(
             &stake_pool_program,
